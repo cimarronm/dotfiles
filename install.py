960825fac435985892c7c8bfc01d48087ad1272f
Cimarron Mittelsteadt <cimarronm@gmail.com>
2013-05-21 15:57:19 -0700

    Added install script to install files to home directory.

---
+++ install.py
@@ -0,0 +1,19 @@
+import os
+
+ignoredfiles = set((".git",__file__))
+
+def installfile(file):
+    os.symlink(os.path.relpath(file, start=os.path.expanduser("~")),
+            os.path.join(os.path.expanduser("~"), file))
+    print("{} installed".format(file))
+
+for file in os.listdir():
+    if file in ignoredfiles:
+        continue
+    try:
+        if os.path.samefile(file, os.path.expanduser(os.path.join("~",file))):
+            print("{} is already installed".format(file))
+        else:
+            print("{} is different".format(file))
+    except FileNotFoundError:
+        installfile(file)
