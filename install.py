3bcd38dea3975b95fbc4026785bfa5d0be3531dc
Cimarron Mittelsteadt <cimarronm@gmail.com>
2014-08-25 11:39:01 -0700

    ENH: Major overhaul to install.py script. Added support for python 2.7. Added logging, executable arguments. Also updated to be import friendly.

--- install.py
+++ install.py
@@ -1,25 +1,68 @@
+#!/usr/local/bin/python3
+
+from __future__ import print_function
+
 import os
+import sys
+import logging
 
-ignoredfiles = set((".git",".gitignore",__file__))
+log = logging.getLogger(__name__)
+ignoredfiles = set((".git", ".gitignore", os.path.basename(__file__)))
 
-def installfile(file):
-    os.symlink(os.path.relpath(file, start=os.path.expanduser("~")),
-            os.path.join(os.path.expanduser("~"), file))
-    print("{} installed".format(file))
 
-for file in os.listdir():
-    errors = False
-    if file in ignoredfiles:
-        continue
+def installfile(file, installdir):
     try:
-        if os.path.samefile(file, os.path.expanduser(os.path.join("~",file))):
-            print("{} is already installed".format(file))
-        else:
-            print("{} is different".format(file))
-            errors = True
-    except FileNotFoundError:
-        installfile(file)
+        os.symlink(os.path.relpath(file, start=installdir),
+               os.path.join(installdir, file))
+    except:
+        log.exception("Problem creating symbolic link")
 
-    if errors:
-        import sys
+def do_install(installdir=os.path.expanduser("~"), dryrun=False):
+    if not os.path.isdir(installdir):
+        log.error("Cannot install to {}. It is not a directory".format(installdir))
+        raise RuntimeError("installdir is not directory")
+
+    installs = []
+    installed = []
+    conflicts = []
+    for file in os.listdir('.'):
+        if file in ignoredfiles:
+            continue
+        try:
+            if os.path.samefile(file, os.path.join(installdir, file)):
+                installed.append(file)
+                log.info("{} is already installed".format(file))
+            else:
+                log.warning("Conflicting file {}".format(file))
+                conflicts.append(file)
+        except OSError:  # FileNotFoundError: OSError instead for python 2 compatibility
+            if not dryrun:
+                installfile(file, installdir)
+            installs.append(file)
+            print("{} installed".format(file))
+
+    return dict(installs=installs, installed=installed, conflicts=conflicts)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    defaultinstall = os.path.expanduser("~")
+    parser.add_argument("installpath",
+                        help="Path to install symbolic links (default:{})".format(defaultinstall),
+                        default=defaultinstall, nargs='?', type=os.path.expanduser)
+    parser.add_argument("--log", help="log level", type=int)
+    parser.add_argument("-n", "--dryrun", help="Perform dry run, don't actually do anything",
+                        action="store_true")
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log)
+    try:
+        results = do_install(installdir=args.installpath, dryrun=args.dryrun)
+    except:
+        errors = True
+    else:
+        errors = False
+
+    if errors or results['conflicts']:
         sys.exit(1)
